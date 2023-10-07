a840379667f7ee667b1ee572feb1bb7cd3d2f7d2
Flashminat0 <wyasaslowe@gmail.com>
2023-10-07 16:16:34 +0530

    axios interceptor done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import axios from "axios";
 
-const App = () => (
-    <h1 className="text-3xl font-bold underline">
-        Hello world!
-    </h1>
-);
+const App = () => {
+    useEffect(() => {
+        axios.get('/api/user')
+            .then(response => {
+                console.log(response)
 
+                // setUserData(response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching user data:', error);
+            });
+    }, []);
+
+    return (
+        <h1 className="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+    );
+}
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import axios from "axios";
+
+
+axios.defaults.baseURL = `${import.meta.env.VITE_BACKEND_URL}`;
+
+axios.interceptors.request.use(
+    config => {
+        config.headers['Accept'] = '*/*';
+        config.headers['Content-Type'] = 'application/json';
+        return config;
+    });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
 )
